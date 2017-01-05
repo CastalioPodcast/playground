6036896ddaaeeb937c649971ffc5464c363fce36
Og Maciel <omaciel@ogmaciel.com>
2017-01-05 14:56:53 -0500

    Minor improvements to the look and feel of grid
    TreeView is now aligned with main frame and columns lined up correctly.

--- tkcastalio.py
+++ tkcastalio.py
@@ -5,18 +5,24 @@ from tkinter import ttk
 class MainWindow(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        self.grid(column=0, row=0, sticky=tk.W)
+        self.grid(column=0, row=0, sticky=(tk.W, tk.W, tk.E, tk.S))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         self.master.title('Castalio Podcast')
 
         self.label = ttk.Label(self, text="")
         self.label.grid(column=0, row=0)
 
-        self.tree = ttk.Treeview(self, columns=('name', 'date'))
-        self.tree.heading('#0', text="Name", anchor=tk.W)
-        self.tree.heading('#1', text="Date", anchor=tk.W)
+        self.tree = ttk.Treeview(self, columns=('date',))
+        self.tree.heading('#1', text="Date")
         self.tree.insert('', 0, 'episodes', text='Episodes', open=True)
-        self.tree.insert('episodes', 1, values=('Episode 82', 'Jan. 8, 2017'))
-        self.tree.grid(column=0, row=2, sticky=tk.W)
+        self.tree.insert(
+            'episodes',
+            1,
+            text='Episode 82',
+            values=('Jan. 8, 2017',)
+        )
+        self.tree.grid(column=0, row=0, sticky=(tk.W, tk.W, tk.E, tk.S))
 
         self.pack(fill=tk.X)
 
@@ -24,3 +30,4 @@ class MainWindow(tk.Frame):
 win = tk.Tk()
 app = MainWindow(master=win)
 app.mainloop()
+
